546b843f6274c1308cb3c515db958a7f9702098b
bi0punk <systecinfo_cl@criptext.com>
2023-04-29 19:06:55 -0400

    primer

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+# Directorio a organizar
+dir_path = "/home/sysbot/Descargas/"
+
+# Crear un diccionario vacío para almacenar los directorios
+dirs = {}
+
+# Recorrer todos los archivos en el directorio
+for file_name in os.listdir(dir_path):
+    # Obtener la extensión del archivo
+    extension = os.path.splitext(file_name)[1]
+
+    # Si la extensión no está en el diccionario, crear un nuevo directorio para ella
+    if extension not in dirs:
+        dirs[extension] = os.path.join(dir_path, extension[1:])
+        os.makedirs(dirs[extension], exist_ok=True)
+
+    # Mover el archivo al directorio correspondiente
+    shutil.move(os.path.join(dir_path, file_name), os.path.join(dirs[extension], file_name))
+print("Archivos organizados con éxito.")
