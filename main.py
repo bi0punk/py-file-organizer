5752a06b8b5b659f13781de13906e0c42e0385d7
bi0punk <systecinfo_cl@criptext.com>
2023-04-29 22:49:56 -0400

    add progressbar and pylint

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
+
+#importamos librerias
 import os
+
 import shutil
 from time import sleep
 from progressbar import progressbar
 
 
 # Directorio a organizar
-dir_path = "/home/sysbot/Descargas/"
+disorder_directory_files = "/home/sysbot/Descargas/"
 
 # Crear un diccionario vacío para almacenar los directorios
 dirs = {}
 
 # Recorrer todos los archivos en el directorio
-for file_name in os.listdir(dir_path):
+for file_name in os.listdir(disorder_directory_files):
     # Obtener la extensión del archivo
     extension = os.path.splitext(file_name)[1]
 
     # Si la extensión no está en el diccionario, crear un nuevo directorio para ella
     if extension not in dirs:
-        dirs[extension] = os.path.join(dir_path, extension[1:])
+        dirs[extension] = os.path.join(disorder_directory_files, extension[1:])
         os.makedirs(dirs[extension], exist_ok=True)
 
     # Mover el archivo al directorio correspondiente
-    shutil.move(os.path.join(dir_path, file_name), os.path.join(dirs[extension], file_name))
+    shutil.move(os.path.join(disorder_directory_files, file_name), 
+                os.path.join(dirs[extension], file_name))
 
 
 for i in progressbar(range(100)):
