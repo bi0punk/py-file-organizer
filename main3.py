a35532f8540168eb726a28e988a96385cef733e7
bi0punk <systecinfo_cl@criptext.com>
2023-06-23 15:53:51 -0400

    work auto update

---
+++ main3.py
@@ -0,0 +1,15 @@
+import os
+
+def listar_directorios_principales(ruta):
+    directorios_principales = []
+    for elemento in os.listdir(ruta):
+        ruta_elemento = os.path.join(ruta, elemento)
+        if os.path.isdir(ruta_elemento) and not elemento.startswith('.'):
+            directorios_principales.append(elemento)
+    return directorios_principales
+
+ruta_base = "/home/sysbot/"
+directorios_principales = listar_directorios_principales(ruta_base)
+
+for directorio in directorios_principales:
+    print(directorio)
