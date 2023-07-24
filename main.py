bc36ce52b1666b0173a1dc4d0ae692ff07a6c89f
bi0punk <systecinfo_cl@criptext.com>
2023-07-23 21:44:33 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
-
-#importamos librerias
 import os
-
 import shutil
-from time import sleep
-from progressbar import progressbar
-
 
 # Directorio a organizar
 disorder_directory_files = "/home/sysbot/Descargas/"
 
 # Crear un diccionario vacío para almacenar los directorios
-dirs = {}
+extension_dirs = {}
 
 # Recorrer todos los archivos en el directorio
 for file_name in os.listdir(disorder_directory_files):
-    # Obtener la extensión del archivo
     try:
+        # Obtener la extensión del archivo
         extension = os.path.splitext(file_name)[1]
 
         # Si la extensión no está en el diccionario, crear un nuevo directorio para ella
-        if extension not in dirs:
-            dirs[extension] = os.path.join(disorder_directory_files, extension[1:])
-            os.makedirs(dirs[extension], exist_ok=True)
+        if extension:
+            target_dir = os.path.join(disorder_directory_files, extension[1:])
+            if not os.path.exists(target_dir):
+                os.makedirs(target_dir)
 
-        # Mover el archivo al directorio correspondiente
-        shutil.move(os.path.join(disorder_directory_files, file_name), 
-                    os.path.join(dirs[extension], file_name))
-    except:
-        print("error")
+            # Mover el archivo al directorio correspondiente
+            shutil.move(os.path.join(disorder_directory_files, file_name), 
+                        os.path.join(target_dir, file_name))
 
-for i in progressbar(range(100)):
-    sleep(0.02)
+    except Exception as e:
+        print(f"Error al organizar el archivo '{file_name}': {str(e)}")
+
 print("Archivos organizados con éxito.")
